90c549aeabc5caf3d911675593037846a24df649
koshalex69 <alex.alex.kosh@gmail.com>
2018-07-16 16:20:41 +0300

    Update EmployeesTableSeeder.php

--- database/seeds/EmployeesTableSeeder.php
+++ database/seeds/EmployeesTableSeeder.php
@@ -22,49 +22,49 @@ class EmployeesTableSeeder extends Seeder
             'depth' => '0',
             'photo' => 'default.jpg'
         ]);
-        // DB::table('employees')->insert([
-        //     'name' => 'John Winchester',
-        //     'position' => 'alpha',
-        //     'start_day' => '2017-01-01',
-        //     'parent_id' => '1',
-        //     'lb' => '2',
-        //     'rb' => '9',
-        //     'salary' => '600',
-        //     'depth' => '1',
-        //     'photo' => 'default.jpg'
-        // ]);
-        // DB::table('employees')->insert([
-        //     'name' => 'Sam Winchester',
-        //     'position' => 'beta',
-        //     'start_day' => '2017-01-01',
-        //     'parent_id' => '2',
-        //     'lb' => '3',
-        //     'rb' => '6',
-        //     'salary' => '300',
-        //     'depth' => '2',
-        //     'photo' => 'default.jpg'
-        // ]);
-        // DB::table('employees')->insert([
-        //     'name' => 'Dean Winchester',
-        //     'position' => 'beta',
-        //     'start_day' => '2015-01-01',
-        //     'parent_id' => '2',
-        //     'lb' => '7',
-        //     'rb' => '8',
-        //     'salary' => '300',
-        //     'depth' => '2',
-        //     'photo' => 'default.jpg'
-        // ]);
-        // DB::table('employees')->insert([
-        //     'name' => 'Castiel Angel',
-        //     'position' => 'alpha',
-        //     'start_day' => '2015-01-01',
-        //     'parent_id' => '3',
-        //     'lb' => '4',
-        //     'rb' => '5',
-        //     'salary' => '333',
-        //     'depth' => '3',
-        //     'photo' => 'default.jpg'
-        // ]);
+        DB::table('employees')->insert([
+             'name' => 'John Winchester',
+             'position' => 'alpha',
+             'start_day' => '2017-01-01',
+             'parent_id' => '1',
+             'lb' => '2',
+             'rb' => '9',
+             'salary' => '600',
+             'depth' => '1',
+             'photo' => 'default.jpg'
+         ]);
+         DB::table('employees')->insert([
+             'name' => 'Sam Winchester',
+             'position' => 'beta',
+             'start_day' => '2017-01-01',
+             'parent_id' => '2',
+             'lb' => '3',
+             'rb' => '6',
+             'salary' => '300',
+             'depth' => '2',
+             'photo' => 'default.jpg'
+         ]);
+         DB::table('employees')->insert([
+             'name' => 'Dean Winchester',
+             'position' => 'beta',
+             'start_day' => '2015-01-01',
+             'parent_id' => '2',
+             'lb' => '7',
+             'rb' => '8',
+             'salary' => '300',
+             'depth' => '2',
+             'photo' => 'default.jpg'
+         ]);
+         DB::table('employees')->insert([
+             'name' => 'Castiel Angel',
+             'position' => 'alpha',
+             'start_day' => '2015-01-01',
+             'parent_id' => '3',
+             'lb' => '4',
+             'rb' => '5',
+             'salary' => '333',
+             'depth' => '3',
+             'photo' => 'default.jpg'
+         ]);
     }
 }
